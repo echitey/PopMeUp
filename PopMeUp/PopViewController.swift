fff6cfd70c9c536442ea4a7f6c4037bc8fbb697a
Yawo Echitey <yawo@echitey.com>
2017-03-22 16:29:55 -0300

    Added Popup of other ViewController

---
+++ PopMeUp/PopViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PopViewController.swift
+//  PopMeUp
+//
+//  Created by Yawo Echitey on 3/22/17.
+//  Copyright © 2017 Echitey. All rights reserved.
+//
+
+import UIKit
+
+class PopViewController: UIViewController {
+    
+    @IBOutlet weak var popView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        popView.layer.cornerRadius = 10;
+        popView.layer.masksToBounds = true;
+    }
+    
+    @IBAction func btnPressed(_ sender: Any){
+        dismiss(animated: true, completion: nil)
+    }
+
+}
