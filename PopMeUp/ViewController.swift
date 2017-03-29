86ec8db63896546b620cf88cc776f6c6353a1866
Yawo Echitey <yawo@echitey.com>
2017-03-29 16:35:52 -0300

    Added Scene Dock Popup with animation

--- PopMeUp/ViewController.swift
+++ PopMeUp/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        popView.layer.cornerRadius = 20
     }
     
     //Internal PopupView
@@ -33,14 +34,12 @@ class ViewController: UIViewController {
 
     @IBAction func openBounce(_ sender: Any){
         self.popConstraint.constant = 0
+        self.btnCancel.alpha = 0.85
         
-        UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: .curveEaseIn, animations: {
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: .curveEaseIn, animations: {
             self.view.layoutIfNeeded()
         }, completion: nil)
         
-        UIView.animate(withDuration: 0.2) {
-            self.btnCancel.alpha = 0.85
-        }
     }
  
     @IBAction func btnCancelAction(_ sender: Any) {
@@ -55,18 +54,37 @@ class ViewController: UIViewController {
     }
     
     //Scene Dock
-    @IBAction func scneDockPop(_ sender: UIButton) {
+    @IBAction func sceneDockPop(_ sender: UIButton) {
         popView.center = view.center
         view.addSubview(popView)
         self.btnCancel.alpha = 0.85
     }
+    
+    //Scene Dock with Effect
     @IBAction func sceneDockPopEffect(_ sender: UIButton) {
+        popView.center = view.center
         
+        //Transform to give more effect
+        popView.transform = CGAffineTransform(scaleX: 0.7, y: 1.3)
+        view.addSubview(popView)
+        
+        UIView.animate(withDuration: 0.2, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: [], animations: {
+            self.popView.transform = .identity
+            self.btnCancel.alpha = 0.85
+        })
     }
     
+    
+    //Animation on Scene Dock Popup Close
     @IBAction func popViewClose(_ sender: UIButton) {
-        self.btnCancel.alpha = 0
-        popView.removeFromSuperview()
+        
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: [], animations: { 
+            self.btnCancel.alpha = 0
+            self.popView.transform = CGAffineTransform(scaleX: 0.2, y: 0.2)
+        }) { (succes) in
+            self.popView.removeFromSuperview()
+            self.popView.transform = .identity
+        }
     }
 }
 
